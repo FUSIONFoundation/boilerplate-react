8f29c5bc6b3e71b127bf0e34815b7e603f3e37a1
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-11-25 09:30:38 -0500

    Added fusion logo

---
+++ src/Components/Fusion.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Fusion extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <input placeholder={'Private Key'}></input>
+            </div>
+        )
+    }
+}
+
+export default Fusion;
